7a5a2d30abd1001dad6c75a9a82c31c288affcfd
Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>
2025-06-03 13:53:08 +0530

    Update AboutUs.jsx

--- src/pages/HomePage/AboutUs.jsx
+++ src/pages/HomePage/AboutUs.jsx
@@ -4,6 +4,7 @@ const AboutUs = () => {
   return (
     <div>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, illo temporibus iure atque minima quasi laborum et explicabo a! Dolorum iste soluta odit suscipit itaque sequi id incidunt aut ratione?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem excepturi dolore architecto aut unde, distinctio reprehenderit consectetur minima quis quia illo error, veritatis exercitationem cumque doloribus ipsum amet ut fugiat.
     </div>
   )
 }
