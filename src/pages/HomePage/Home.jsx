cf3ff2c38f42faa515ecc449bd9a9e431edb79a7
Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>
2025-06-03 15:14:40 +0530

    update

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className='home-wrapper'>
+            <div className='darkness'></div>
+
+            <div className='text-center'>
+                <h1 className='text-5xl'>Your Mental Wellness Matters</h1>
+                <p className='text-xl'>A safe space for students, staff, and families of the university to receive <br /> compassionate psychological support and care.</p>
+            </div>
+        </section>
     )
 }
 
